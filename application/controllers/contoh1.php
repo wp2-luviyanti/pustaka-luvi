44ac19b3b53f68b39e2f43d68a0ccb8546b78c39
luviyanti <luviyanti65@gmail.com>
2023-09-25 16:16:58 +0700

    pustaka-luvi

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan saya </h1>";
+        echo "Nama saaya Luviyanti hobi saya rebahan 
+            nim saya 19220352 ";
+    }
+}
