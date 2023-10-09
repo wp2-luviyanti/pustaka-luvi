f8fe28d3b3ec2b533cbfe17c29edf70e01e372e5
luviyanti <luviyanti65@gmail.com>
2023-10-09 16:45:01 +0700

    latihan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Luviyanti</li>
+ <li>Nama Belakang : Luviya</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalanin Aja Yakan </li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li></li>Di RS Bersalin
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Tidur</li>
+ <li>Makan</li>
+ </ul>
+</section>
